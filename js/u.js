fdc5cb3701517342d5f1d651c1776798d4b52e10
fzdm <johndoe@example.com>
2019-06-26 01:07:01 +0800

    fix bug

--- js/u.js
+++ js/u.js
@@ -1,77 +1,83 @@
 var ock = getCookie("picHost");
 if (ock == "") {
-document.writeln("<div class=\'speed\' data-url=\'http://p1.manhuapan.com\'></div>");
-document.writeln("<div class=\'speed\' data-url=\'http://p5.manhuapan.com\'></div>");
-document.writeln("<div class=\'speed\' data-url=\'http://p2.manhuapan.com\'></div>");
-document.writeln("<div class=\'speed\' data-url=\'http://p3.manhuapan.com\'></div>");
-document.writeln("<div class=\'speed\' data-url=\'http://p17.manhuapan.com\'></div>");
-document.writeln("<div class=\'speed\' data-url=\'http://www-mipengine-org.mipcdn.com/i/p1.manhuapan.com/\'></div>");
+    document.writeln("<div class=\'speed\' data-url=\'http://p1.manhuapan.com\'></div>");
+    document.writeln("<div class=\'speed\' data-url=\'http://www-mipengine-org.mipcdn.com/i/p1.manhuap" +
+            "an.com/\'></div>");
+    document.writeln("<div class=\'speed\' data-url=\'http://p5.manhuapan.com\'></div>");
+    document.writeln("<div class=\'speed\' data-url=\'http://p2.manhuapan.com\'></div>");
+    document.writeln("<div class=\'speed\' data-url=\'http://p3.manhuapan.com\'></div>");
+    document.writeln("<div class=\'speed\' data-url=\'http://p17.manhuapan.com\'></div>");
 
-$(function() {
-    var obj_n;
-    var start_time;
-    var ss=[]
-    var mhserver =["p1.manhuapan.com","p2.manhuapan.com","p3.manhuapan.com","p5.manhuapan.com","p17.manhuapan.com","www-mipengine-org.mipcdn.com/i/p1.manhuapan.com"]
-    var picLoad = function() {
-        var end_time = new Date().getTime(); // 結束時間
-        //var pass_time = (end_time - start_time) + ' 毫秒';
-        ss.unshift(end_time - start_time);
-        //$('.speed').eq(obj_n).html(pass_time);
-        nextTest();
-    };
-    var picError = function() {
-        ss.unshift(99999)
-        //$('.speed').eq(obj_n).html("無法連結");
-        nextTest();
-    };
-    var nextTest = function() {
-        ++obj_n;
-        //$('.speed').eq(obj_n).html('測速中...');
-        setTimeout(function() {
-            testSpeed();
-        }, 300);
-    };
+    $(function () {
+        var obj_n;
+        var start_time;
+        var ss = []
+        var mhserver = [
+            "p1.manhuapan.com",
+            "www-mipengine-org.mipcdn.com/i/p1.manhuapan.com",
+            "p2.manhuapan.com",
+            "p3.manhuapan.com",
+            "p5.manhuapan.com",
+            "p17.manhuapan.com"
+        ]
+        var picLoad = function () {
+            var end_time = new Date().getTime(); // 結束時間
+            var pass_time = (end_time - start_time) + ' 毫秒';
+            ss.push(end_time - start_time);
+            //$('.speed').eq(obj_n).html(pass_time);
+            nextTest();
+        };
+        var picError = function () {
+            ss.push(99999)
+            //$('.speed').eq(obj_n).html("無法連結");
+            nextTest();
+        };
+        var nextTest = function () {
+            ++obj_n;
+            //$('.speed').eq(obj_n).html('測速中...');
+            setTimeout(function () {
+                testSpeed();
+            }, 300);
+        };
 
-    function indexOfSmallest(a) {
-        var lowest = 0;
-        for (var i = 1; i < a.length; i++) {
-            if (a[i] < a[lowest]) lowest = i;
+        function indexOfSmallest(a) {
+            return a.indexOf(Math.min.apply(Math, a));
         }
-    return lowest;
-}
 
-    var testSpeed = function() {
-        var obj = $('.speed').eq(obj_n);
-        //console.log(obj.data('url'));
-        
-        if (obj.length != 1) {
-            Array.prototype.min = function(){ 
-return Math.min.apply({},this) ;
-}
+        var testSpeed = function () {
+            var obj = $('.speed').eq(obj_n);
+            //console.log(obj.data('url'));
+
+            if (obj.length != 1) {
+                Array.prototype.min = function () {
+                    return Math
+                        .min
+                        .apply({}, this);
+                }
+                // obj.html(mhserver[indexOfSmallest(ss)]);
+                // console.log(ss,obj.data('url'),mhserver[indexOfSmallest(ss)]) console.log(ss)
+                // console.log(indexOfSmallest(ss)) console.log(ss[indexOfSmallest(ss)]);
+                addCookie("picHost", mhserver[indexOfSmallest(ss)], 12);
+                return false;
+            }
+            // console.log(obj,obj_n,obj.data('url'))
+            // obj.html(mhserver[indexOfSmallest(ss)]+'測速中...'); //建立 img 元素
+            var pic = document.createElement('img');
+            pic.onload = picLoad;
+            pic.onerror = picError;
+            start_time = new Date().getTime(); // 開始時間
+            var m = 100;
+            var n = Math.floor(Math.random() * m);
+            if (obj.data('url').indexOf("mipengine") != -1) {
+                n = 1;
+            }
+            pic.src = obj.data('url') + '/2017/02/148816883875316.jpg?' + n;
             //console.log(ss)
-            //console.log(indexOfSmallest(ss))
-            //console.log(ss[indexOfSmallest(ss)]);
-            addCookie("picHost", mhserver[indexOfSmallest(ss)],12);
-            return false;
-        }
-        //obj.html('測速中...');
-        // 建立 img 元素
-        var pic = document.createElement('img');
-        pic.onload = picLoad;
-        pic.onerror = picError;
-        start_time = new Date().getTime(); // 開始時間
-        var m=100;
-        var n=Math.floor(Math.random()*m);
-        if(obj.data('url').indexOf("mipengine")!= -1){
-            n = 1;
-        }
-        pic.src = obj.data('url') + '/2017/02/148816883875316.jpg?'+n;
-        //console.log(ss)
-    };
-    $(function() {
-        obj_n = 0;
-        testSpeed();
-    });
+        };
+        $(function () {
+            obj_n = 0;
+            testSpeed();
+        });
 
-});
+    });
 };
