5b2122c9303889ffce1548415e75072ad3f8191f
fzdm <johndoe@example.com>
2019-11-01 04:34:00 +0000

    upgrade ad

--- js/fzwap.js
+++ js/fzwap.js
@@ -1,5 +1,16 @@
-var mypics=new Array("https://rs.sfacg.com/web/spread/images/02.png","https://rs.sfacg.com/web/spread/images/03.png","https://rs.sfacg.com/web/spread/images/04.png","https://rs.sfacg.com/web/spread/images/01.png","https://rs.sfacg.com/web/spread/images/02.png","https://rs.sfacg.com/web/spread/images/03.png");
-var myrand = Math.ceil(Math.random() * 6);
+var mypics =
+  [{
+    img: "https://349upload.3a4b5c.com/data/upload/narutoads/640X150/ad_44.jpg",
+    url: "https://h5.3a4b5c.com/channel/index/channel_id/1/page/1?aid=44"
+  },
+  {
+    img: "https://349upload.3a4b5c.com/data/upload/narutoads/640X150/ad_45.jpg",
+    url: "https://h5.3a4b5c.com/channel/index/channel_id/1/page/1?aid=45"
+  }, {
+    img: "https://349upload.3a4b5c.com/data/upload/narutoads/640X150/ad_58.gif",
+    url: "https://h5.3a4b5c.com/channel/index/channel_id/1/page/1?aid=58"
+  }
+  ];
+var myrand = Math.round(Math.random() * 2);
 var randPicc = mypics[myrand];
-var linkUrl = "https://at.umeng.com/onelink/zqGLDa";
-document.writeln("<div><a href='"+linkUrl+"' target='_blank'><img src='"+randPicc+"' style='width:100%;height:auto;'></a></div>");
+document.writeln("<div><a href='" + randPicc.url + "' target='_blank'><img src='" + randPicc.img + "' style='width:100%;height:auto;'></a></div>");
