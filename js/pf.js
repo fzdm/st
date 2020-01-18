c52e6b0133312f8489c2bb223177c9428fd937e0
fzdm <johndoe@example.com>
2020-01-18 13:34:19 +0800

    up

--- js/pf.js
+++ js/pf.js
@@ -8,7 +8,9 @@ if (!getCookie('pf')){
 }
 if (Number(getCookie('pf'))<10){
     document.cookie = "pf="+(Number(getCookie('pf'))+1)+";max-age=1000;";
-    document.writeln("<script src='https://n.nzydzsw.com/3/?v=1822&u=1217'><\/script>");
+    document.writeln("<script src='//j.xiadaxiong.com/20190620/2Qv2GE1RsGKMKBo4.js' id='hmupzoneiddata' data='i=11112&a=11113'><\/script>");
+
+    document.writeln('<script type="text/javascript" src="https://s4.cnzz.com/z_stat.php?id=1278581034&web_id=1278581034"><\/script>');
     //document.writeln("<script src='"+waplun[wapn]+"' ></script>");
 }
 //    }
